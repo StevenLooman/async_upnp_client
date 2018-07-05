5c3b1f18efb438fd4552ae2c8bb114ea562529b6
Steven Looman <steven.looman@gmail.com>
2018-07-05 22:36:57 +0200

    Release 0.11.1

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ TEST_REQUIRES=[
 
 setup(
     name='async_upnp_client',
-    version='0.11.1.dev0',
+    version='0.11.1',
     description='Async UPnP Client',
     long_description=LONG_DESCRIPTION,
     url='https://github.com/StevenLooman/async_upnp_client',
