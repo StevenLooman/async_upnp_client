1f81721603ce43a9d21c601ddba406edf2ffcec3
J. Nick Koston <nick@koston.org>
2021-05-23 07:06:32 -1000

    Make search use SSDPListener

--- async_upnp_client/search.py
+++ async_upnp_client/search.py
@@ -4,11 +4,12 @@ import asyncio
 import logging
 from asyncio import DatagramTransport
 from asyncio.events import AbstractEventLoop
-from ipaddress import IPv4Address
+from ipaddress import IPv4Address, IPv6Address
 from typing import Awaitable, Callable, Mapping, MutableMapping, Optional
 
 from async_upnp_client.ssdp import (
     SSDP_IP_V4,
+    SSDP_IP_V6,
     SSDP_MX,
     SSDP_ST_ALL,
     SSDP_TARGET_V4,
@@ -25,6 +26,94 @@ _LOGGER = logging.getLogger(__name__)
 _LOGGER_TRAFFIC_SSDP = logging.getLogger("async_upnp_client.traffic.ssdp")
 
 
+class SSDPListener:
+    """Class to listen for SSDP."""
+
+    def __init__(
+        self,
+        async_callback: Callable[[Mapping[str, str]], Awaitable],
+        loop: Optional[AbstractEventLoop] = None,
+        source_ip: Optional[IPvXAddress] = None,
+        target_ip: Optional[IPvXAddress] = None,
+        timeout: int = SSDP_MX,
+        service_type: str = SSDP_ST_ALL,
+    ) -> None:
+        """Init the ssdp listener class."""
+        self.async_callback = async_callback
+        self.service_type = service_type
+        self.source_ip = source_ip
+        self.target_ip = target_ip
+        self.timeout = timeout
+        self.loop = loop
+        self.target_host: Optional[str] = None
+        self.target: Optional[str] = None
+        self.transport: Optional[DatagramTransport] = None
+
+    def async_search(self) -> None:
+        """Start an SSDP search."""
+        packet = build_ssdp_search_packet(
+            self.target_data, self.timeout, self.service_type
+        )
+        _LOGGER.debug("Sending M-SEARCH packet")
+        _LOGGER_TRAFFIC_SSDP.debug("Sending M-SEARCH packet: %s", packet)
+        assert self.transport is not None
+        self.transport.sendto(packet, self.target)
+
+    async def _async_on_data(
+        self, request_line: str, headers: MutableMapping[str, str]
+    ) -> None:
+        _LOGGER.debug(
+            "Received response, request line: %s, headers: %s", request_line, headers
+        )
+        headers["_source"] = "search"
+        if self.target_host and self.target_host != headers["_host"]:
+            return
+        await self.async_callback(headers)
+
+    async def _async_on_connect(self, transport: DatagramTransport) -> None:
+        self.transport = transport
+        self.async_search()
+
+    async def async_start(self) -> None:
+        """Start the listener."""
+        if self.target_ip is None:
+            if self.source_ip and self.source_ip.version == 6:
+                target_ip: IPvXAddress = IPv6Address(SSDP_IP_V6)
+            else:
+                target_ip = IPv4Address(SSDP_IP_V4)
+        if self.source_ip is None:
+            source_ip = get_source_ip_from_target_ip(target_ip)
+
+        sock, source, self.target = get_ssdp_socket(source_ip, target_ip)
+
+        # We use the standard target in the data of the announce since
+        # many implementations will ignore the request otherwise
+        if target_ip.version == 6:
+            self.target_data = SSDP_TARGET_V6
+        else:
+            self.target_data = SSDP_TARGET_V4
+
+        if not target_ip.is_multicast:
+            self.target_host = get_host_string(self.target)
+        else:
+            self.target_host = ""
+
+        sock.bind(source)
+        loop = self.loop or asyncio.get_running_loop()
+
+        await loop.create_datagram_endpoint(
+            lambda: SsdpProtocol(
+                loop, on_connect=self._async_on_connect, on_data=self._async_on_data
+            ),
+            sock=sock,
+        )
+
+    def async_stop(self) -> None:
+        """Stop the listener."""
+        assert self.transport is not None
+        self.transport.close()
+
+
 async def async_search(
     async_callback: Callable[[Mapping[str, str]], Awaitable],
     timeout: int = SSDP_MX,
@@ -34,55 +123,16 @@ async def async_search(
     target_ip: Optional[IPvXAddress] = None,
 ) -> None:
     """Discover devices via SSDP."""
-    # pylint: disable=too-many-arguments,too-many-locals
+    # pylint: disable=too-many-arguments
     loop_: AbstractEventLoop = loop or asyncio.get_event_loop()
 
-    if target_ip is None:
-        target_ip = IPv4Address(SSDP_IP_V4)
-    if source_ip is None:
-        source_ip = get_source_ip_from_target_ip(target_ip)
-
-    sock, source, target = get_ssdp_socket(source_ip, target_ip)
-
-    # We use the standard target in the data of the announce since
-    # many implementations will ignore the request otherwise
-    if target_ip.version == 6:
-        target_data = SSDP_TARGET_V6
-    else:
-        target_data = SSDP_TARGET_V4
-    sock.bind(source)
-
-    if not target_ip.is_multicast:
-        target_host = get_host_string(target)
-    else:
-        target_host = ""
-
-    async def on_connect(transport: DatagramTransport) -> None:
-        """Handle connection made."""
-        packet = build_ssdp_search_packet(target_data, timeout, service_type)
-        _LOGGER.debug("Sending M-SEARCH packet")
-        _LOGGER_TRAFFIC_SSDP.debug("Sending M-SEARCH packet: %s", packet)
-        transport.sendto(packet, target)
-
-    async def on_data(request_line: str, headers: MutableMapping[str, str]) -> None:
-        """Handle data."""
-        _LOGGER.debug(
-            "Received response, request line: %s, headers: %s", request_line, headers
-        )
-        headers["_source"] = "search"
-        if target_host and target_host != headers["_host"]:
-            return
-        await async_callback(headers)
-
-    # Create protocol and send discovery packet.
-    connect = loop_.create_datagram_endpoint(
-        lambda: SsdpProtocol(loop_, on_connect=on_connect, on_data=on_data),
-        sock=sock,
+    listener = SSDPListener(
+        async_callback, loop_, source_ip, target_ip, timeout, service_type
     )
-    transport, _ = await connect
+
+    await listener.async_start()
 
     # Wait for devices to respond.
     await asyncio.sleep(timeout)
 
-    # Fin.
-    transport.close()
+    listener.async_stop()
