c9026a4c4e049710b7893cf257261d8c70000dc9
Steven Looman <steven.looman@gmail.com>
2022-03-08 19:47:25 +0100

    Try fixing tox build

--- async_upnp_client/__init__.py
+++ async_upnp_client/__init__.py
@@ -3,16 +3,20 @@
 
 __version__ = "0.26.1"
 
-from async_upnp_client.advertisement import SsdpAdvertisementListener  # noqa: F401
-from async_upnp_client.client import UpnpAction  # noqa: F401
-from async_upnp_client.client import UpnpDevice  # noqa: F401
-from async_upnp_client.client import UpnpRequester  # noqa: F401
-from async_upnp_client.client import UpnpService  # noqa: F401
-from async_upnp_client.client import UpnpStateVariable  # noqa: F401
-from async_upnp_client.client_factory import UpnpFactory  # noqa: F401
-from async_upnp_client.event_handler import UpnpEventHandler  # noqa: F401
-from async_upnp_client.event_handler import UpnpNotifyServer  # noqa: F401
-from async_upnp_client.exceptions import UpnpError  # noqa: F401
-from async_upnp_client.exceptions import UpnpValueError  # noqa: F401
-from async_upnp_client.search import SsdpSearchListener  # noqa: F401
-from async_upnp_client.ssdp_listener import SsdpListener  # noqa: F401
+try:
+    from async_upnp_client.advertisement import SsdpAdvertisementListener  # noqa: F401
+    from async_upnp_client.client import UpnpAction  # noqa: F401
+    from async_upnp_client.client import UpnpDevice  # noqa: F401
+    from async_upnp_client.client import UpnpRequester  # noqa: F401
+    from async_upnp_client.client import UpnpService  # noqa: F401
+    from async_upnp_client.client import UpnpStateVariable  # noqa: F401
+    from async_upnp_client.client_factory import UpnpFactory  # noqa: F401
+    from async_upnp_client.event_handler import UpnpEventHandler  # noqa: F401
+    from async_upnp_client.event_handler import UpnpNotifyServer  # noqa: F401
+    from async_upnp_client.exceptions import UpnpError  # noqa: F401
+    from async_upnp_client.exceptions import UpnpValueError  # noqa: F401
+    from async_upnp_client.search import SsdpSearchListener  # noqa: F401
+    from async_upnp_client.ssdp_listener import SsdpListener  # noqa: F401
+except ImportError:
+    # Not what we want, for tox-testing purposes.
+    pass
