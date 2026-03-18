324ced1c6e64749c63dd8a0b3e500577c3402e2f
MacTheFork <pete.mill@gmail.com>
2026-03-18 13:02:39 +0000

    fix 13. Redundant conditional

--- async_upnp_client/profiles/ohmedia.py
+++ async_upnp_client/profiles/ohmedia.py
@@ -1397,7 +1397,7 @@ class OhmDevice(UpnpProfileDevice):
             return None
         action = service.action(action_name)
         result = await action.async_call(**kwargs)
-        return result if result is not None else None
+        return result
 
     async def playlist_last_id(self) -> int:
         """Return the last id of the playlist."""
