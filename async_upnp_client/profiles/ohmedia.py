efcc741ebdcddd7bd216fb30c9367d52ba8571e9
MacTheFork <pete.mill@gmail.com>
2026-03-24 01:47:50 +0000

    prefix async function name with async

--- async_upnp_client/profiles/ohmedia.py
+++ async_upnp_client/profiles/ohmedia.py
@@ -1565,7 +1565,7 @@ class OhmDevice(UpnpProfileDevice):
                 last_id = decoded[-1]
         return last_id
 
-    async def pins_set_device_metadata(self, pin_metadata: dict) -> None:
+    async def async_pins_set_device_metadata(self, pin_metadata: dict) -> None:
         """Set Pins service device using single metadata dictionary.
 
         :param pin_metadata: dictionary containing necessary metadata
